fa9f23c0ca98db73c442c94364b0eb5d2d29aff0
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2026-02-07 21:42:33 +0530

    deploy

---
+++ backend/src/config/index.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+
+type ServerConfig = {
+    PORT: number,
+    GEMINI_KEY:string,
+    LIVEKIT_API_SECRET:string,
+    LIVEKIT_API_KEY:string,
+    OPENAI_API_KEY:string   
+}
+
+function loadEnv() {
+    dotenv.config();
+    console.log(`Environment variables loaded`);
+}
+
+loadEnv();
+
+export const serverConfig: ServerConfig = {
+    PORT: Number(process.env.PORT) || 3001,
+    GEMINI_KEY:process.env.GEMINI_KEY!,
+    LIVEKIT_API_KEY:process.env.LIVEKIT_API_KEY || '',
+    LIVEKIT_API_SECRET:process.env.LIVEKIT_API_SECRET || '',
+    OPENAI_API_KEY:process.env.OPENAI_API_KEY || ''
+};
---
+++ frontend/lib/types.ts
@@ -0,0 +1,19 @@
+export interface Question {
+    title: string;
+    description: string;
+    difficulty: string;
+    exampleInputFirst: string;
+    exampleOutputFirst: string;
+    exampleInputSecond: string;
+    exampleOutputSecond: string;
+    constraints: string[];
+  }
+  export interface SubmissionResult {
+    success: boolean;
+    title:string;
+    description:string;
+    difficulty:string;
+    analysis:string;
+    improvements:string;
+  
+  }
